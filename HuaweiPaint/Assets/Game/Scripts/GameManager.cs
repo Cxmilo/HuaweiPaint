770bb9d00c0308ddf038701d25e72ab8fe9f79db
Camilo3DS <cpineda@teravisiongames.com>
2018-09-18 00:12:44 -0500

    *-Add external assets

---
+++ HuaweiPaint/Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using PaintCraft.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public EasyTween startScreen;
+    public EasyTween instructionScreen;
+
+    public GameObject paintScreen;
+    public CanvasController paintPrefab;
+
+
+    public void OnStartScreenClick ()
+    {
+        startScreen.OpenCloseObjectAnimation();
+        instructionScreen.OpenCloseObjectAnimation();
+    }
+
+    public void OnInstructionScreen ()
+    {
+        instructionScreen.OpenCloseObjectAnimation();
+        paintPrefab.transform.parent.gameObject.SetActive(true);
+        paintScreen.SetActive(true);
+    }
+
+    public void SaveImage ()
+    {
+        takeHiResShot = true;
+    }
+
+
+    bool takeHiResShot = false;
+
+    public int resWidth = 1080;
+    public int resHeight = 1920;
+
+
+    void LateUpdate()
+    {
+        takeHiResShot |= Input.GetKeyDown("k");
+        if (takeHiResShot)
+        {
+            Camera[] cameras = paintPrefab.transform.parent.GetComponentsInChildren<Camera>();
+            Camera camera = cameras[1];
+
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            camera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            camera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            camera.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = ScreenShotName(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            takeHiResShot = false;
+        }
+    }
+
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+                             Application.persistentDataPath,
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+}
