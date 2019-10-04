293d5d587050718ad9d08cbb50983d5277802074
Camilo3ds <camilo3ds@gmail.com>
2019-10-03 23:57:15 -0500

    update banco bogota

---
+++ HuaweiPaint/Assets/Game/Scripts/LoadRandomPaint.cs
@@ -0,0 +1,30 @@
+﻿using PaintCraft.Canvas.Configs;
+using PaintCraft.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadRandomPaint : MonoBehaviour {
+
+
+
+    public CanvasController controller;
+    public PageConfig[] pageConfig;
+    // Use this for initialization
+    void Start () {
+		
+        if(controller)
+        {
+            int randomIndex = Random.Range(0, pageConfig.Length);
+            controller.PageConfig = pageConfig[randomIndex];
+            controller.SetActivePageConfig(controller.PageConfig);
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
